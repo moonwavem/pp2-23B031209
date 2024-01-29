5037eec293162141e39e3d330ac5ce64f1170a58
moonwavem <moonwaveam@gmail.com>
2024-01-29 16:27:36 +0600

    lab2

---
+++ exercise2/2.py
@@ -0,0 +1,36 @@
+print(10 *5)
+print(10 / 2)
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+if 5 !=10:
+  print("5 and 10 is not equal")
+
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,"lemon")
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
